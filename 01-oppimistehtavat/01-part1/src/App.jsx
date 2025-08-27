04fd1f53043997af66fa338801c99abc331052ec
Alessio2002 <alessio.zanasi02@gmail.com>
2025-08-27 12:33:07 +0300

    first commit of counter

---
+++ 01-oppimistehtavat/01-part1/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import { useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => setCounter(counter + 1), 1000);
+
+  console.log("rendering...", counter);
+
+  return (
+    <>
+      <p>{counter}</p>
+    </>
+  );
+};
+
+export default App;
